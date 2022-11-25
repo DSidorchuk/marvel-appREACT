8d9a2ea5aeb06d49824cef0a5d4bba734e02cd64
Daniil <sidorchuk.daniil@gmail.com>
2022-11-25 20:54:58 +0200

    Commit 2. CharList renders by data from marvel API

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=ecaa5dcf345cfec636212d7649093a0a';
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -13,8 +14,8 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -32,13 +33,18 @@ class MarvelService {
             descr = descr.substr(0, 209) + '...';
         }
         return {
+            id: char.id,
             name: char.name,
             description: descr,
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
 
 export default MarvelService;
+
+
+//"http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
